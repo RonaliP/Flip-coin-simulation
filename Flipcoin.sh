875d26cef32a0b87bc4f179dc316cd6777cbac86
Ronali <ronalipanigrahy88@gmail.com>
2020-10-18 19:06:34 +0530

    PREVIOUS CODE MODIFIED WHERE THE LOOP WILL CONTINUE TILL HEAD OR TAIL WINS 21 TIMES

--- Flipcoin.sh
+++ Flipcoin.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
 echo "WELCOME TO FLIP-COIN-SIMULATION"
 
-read -p "HOW MANY TIMES YOU WANT TO FLIP COIN" Number
 head=0
 tail=0
-for((i=0;i<$Number;i++))
+while(true)
 do
 
 	winner=$((RANDOM%2))
@@ -11,10 +11,19 @@ do
 	if [ $winner -eq 1 ]
 	then
 		head=$(($head+1))
-	else
+		if [ $head == 21 ]
+		then
+			echo "HEAD WON 21 TIMES"
+			break
+		fi
+	elif [ $winner -eq 0 ]
+	then
 		tail=$(($tail+1))
+		if [ $tail == 21 ]
+		then
+			echo "TAIL WON 21 TIMES"
+			break
+		fi
 	fi
 done
 
-echo "HEAD WON $head times"
-echo "TAIL WON $tail TIMES"
