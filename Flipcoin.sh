00a85f047bbfe47015b93bb5252dc8b6f489aa99
Ronali <ronalipanigrahy88@gmail.com>
2020-10-18 18:51:45 +0530

    CODE FOR COIN FLIP THROUGH LOOPING ADDED

--- Flipcoin.sh
+++ Flipcoin.sh
@@ -1,10 +1,20 @@
 echo "WELCOME TO FLIP-COIN-SIMULATION"
 
-winner=$((RANDOM%2))
+read -p "HOW MANY TIMES YOU WANT TO FLIP COIN" Number
+head=0
+tail=0
+for((i=0;i<$Number;i++))
+do
 
-if [ $winner -eq 1 ]
-then
-	echo "WINNER IS HEAD"
-else
-	echo "WINNER IS TAIL"
-fi
+	winner=$((RANDOM%2))
+
+	if [ $winner -eq 1 ]
+	then
+		head=$(($head+1))
+	else
+		tail=$(($tail+1))
+	fi
+done
+
+echo "HEAD WON $head times"
+echo "TAIL WON $tail TIMES"
